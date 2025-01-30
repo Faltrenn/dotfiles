50a6d3aeba657cf4213887d3e91a66663e10ea56
manel <manelzaum@icloud.com>
2025-01-30 15:56:07 -0300

    add: treesitter.

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return { -- Better syntax highlighting
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+}
+
