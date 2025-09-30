c7fcfe1dabe050ac7b867a48ffd91f312562f36a
manel <manelzaum@icloud.com>
2025-09-30 14:52:37 -0300

    some keybinds and editor config.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,50 @@
 require("config.lazy")
 
-vim.cmd.colorscheme "catppuccin"
+_G.vim = vim -- Fix an annoying error undefined global 'vim'
+
+-- Theme
+vim.cmd.colorscheme("catppuccin") -- Define Theme
+
+-- Editor configs
+vim.opt.expandtab = true -- Convert TABs in spaces
+vim.opt.shiftwidth = 2 -- Number of spaces in each identation level
+vim.opt.tabstop = 2 -- Number of spaces the a tab represents
+vim.opt.softtabstop = 2 -- Controla a remoção de espaços ao pressionar <BS>
+vim.opt.number = true -- Enumerate lines
+vim.opt.undofile = true -- Enable undo even after close editor
+vim.opt.scrolloff = 10 -- Set line offset in scroll
+vim.opt.colorcolumn = "88" -- Set a different color to the character 88 of each line
+vim.opt.mouse = "" -- Disable mouse actions
+vim.opt.ignorecase = true
+
+-- Keybindings
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>") -- Close search hover
+vim.keymap.set("n", "<leader>pq", "<cmd>Ex<CR>") -- Close file without close editor
+vim.keymap.set(
+	"n",
+	"[d",
+	vim.diagnostic.goto_prev,
+	{ desc = "Go to previous [D]iagnostic message" }
+)
+vim.keymap.set(
+	"n",
+	"]d",
+	vim.diagnostic.goto_next,
+	{ desc = "Go to next [D]iagnostic message" }
+)
+
+vim.keymap.set(
+	"n",
+	"<leader>q",
+	vim.diagnostic.setloclist,
+	{ desc = "Open diagnostic [Q]uickfix list" }
+)
+
+-- Aliases
+vim.api.nvim_create_user_command("W", "w", {})
+vim.api.nvim_create_user_command("Q", "q", {})
+vim.api.nvim_create_user_command("Wq", "wq", {})
+vim.api.nvim_create_user_command("WQ", "wq", {})
+vim.api.nvim_create_user_command("Wqa", "wqa", {})
+vim.api.nvim_create_user_command("WQa", "wqa", {})
+vim.api.nvim_create_user_command("LAzy", "Lazy <args>", { nargs = "?" })
