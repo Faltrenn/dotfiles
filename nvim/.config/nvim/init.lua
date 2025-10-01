a21b29f133a0e4cb74ad2d3db91c9bd9b62729aa
manel <manelzaum@icloud.com>
2025-10-01 11:20:03 -0300

    add some stuff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,38 +6,38 @@ _G.vim = vim -- Fix an annoying error undefined global 'vim'
 vim.cmd.colorscheme("catppuccin") -- Define Theme
 
 -- Editor configs
-vim.opt.expandtab = true -- Convert TABs in spaces
-vim.opt.shiftwidth = 2 -- Number of spaces in each identation level
-vim.opt.tabstop = 2 -- Number of spaces the a tab represents
-vim.opt.softtabstop = 2 -- Controla a remoção de espaços ao pressionar <BS>
-vim.opt.number = true -- Enumerate lines
-vim.opt.undofile = true -- Enable undo even after close editor
-vim.opt.scrolloff = 10 -- Set line offset in scroll
+vim.opt.expandtab = true   -- Convert TABs in spaces
+vim.opt.shiftwidth = 2     -- Number of spaces in each identation level
+vim.opt.tabstop = 2        -- Number of spaces the a tab represents
+vim.opt.softtabstop = 2    -- Controla a remoção de espaços ao pressionar <BS>
+vim.opt.number = true      -- Enumerate lines
+vim.opt.undofile = true    -- Enable undo even after close editor
+vim.opt.scrolloff = 10     -- Set line offset in scroll
 vim.opt.colorcolumn = "88" -- Set a different color to the character 88 of each line
-vim.opt.mouse = "" -- Disable mouse actions
+vim.opt.mouse = ""         -- Disable mouse actions
 vim.opt.ignorecase = true
 
 -- Keybindings
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>") -- Close search hover
-vim.keymap.set("n", "<leader>pq", "<cmd>Ex<CR>") -- Close file without close editor
+vim.keymap.set("n", "<leader>pq", "<cmd>Ex<CR>")    -- Close file without close editor
 vim.keymap.set(
-	"n",
-	"[d",
-	vim.diagnostic.goto_prev,
-	{ desc = "Go to previous [D]iagnostic message" }
+  "n",
+  "[d",
+  vim.diagnostic.goto_prev,
+  { desc = "Go to previous [D]iagnostic message" }
 )
 vim.keymap.set(
-	"n",
-	"]d",
-	vim.diagnostic.goto_next,
-	{ desc = "Go to next [D]iagnostic message" }
+  "n",
+  "]d",
+  vim.diagnostic.goto_next,
+  { desc = "Go to next [D]iagnostic message" }
 )
 
 vim.keymap.set(
-	"n",
-	"<leader>q",
-	vim.diagnostic.setloclist,
-	{ desc = "Open diagnostic [Q]uickfix list" }
+  "n",
+  "<leader>q",
+  vim.diagnostic.setloclist,
+  { desc = "Open diagnostic [Q]uickfix list" }
 )
 
 -- Aliases
