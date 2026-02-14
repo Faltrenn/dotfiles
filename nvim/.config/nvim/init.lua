3eec6da95a4277f9c8f8ee3566ed1b98b965e173
manel <manelzaum@icloud.com>
2026-02-13 21:09:06 -0300

    add: change working directory to neogit.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -27,6 +27,21 @@ vim.keymap.set( "n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D
 vim.keymap.set( "n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set( "n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
+NEOGIT_DIR = require("oil").get_current_dir()
+
+vim.keymap.set("n", "<leader>cwd", function()
+    NEOGIT_DIR = require("oil").get_current_dir()
+    if not NEOGIT_DIR then
+        NEOGIT_DIR = vim.fn.expand("%:p:h")
+    end
+    print("Neogit Working Directory setted to", NEOGIT_DIR)
+end, { desc = "Change neogit working directory" })
+
+vim.keymap.set({"n", "v"}, "<leader>gg", function ()
+    require("neogit").open({ cwd=NEOGIT_DIR })
+end, { desc = "Show Neogit UI" })
+
+
 -- Aliases
 vim.api.nvim_create_user_command("W", "w", {})
 vim.api.nvim_create_user_command("Q", "q", {})
