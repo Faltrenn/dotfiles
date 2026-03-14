1a4c1c685b0b949f7a526e26fdf7c7e81a49e71b
manel <manelzaum@icloud.com>
2026-03-14 13:39:55 -0300

    add: treesitter.

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+    opts = {
+        auto_install = true,
+        indent = { enable = true },
+
+        ignore_install = { "latex" },
+        highlight = {
+            enable = true,
+
+            disable = { "latex" },
+        },
+    }
+}
