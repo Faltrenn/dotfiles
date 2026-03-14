478a2e68f6d8af030212a6131b7e648a4f56dd3d
manel <manelzaum@icloud.com>
2026-03-14 14:13:38 -0300

    add change directory of neogit.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -10,4 +10,20 @@ vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a T
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
-vim.o.undofile = true
+vim.o.undofile = true -- Preserves undo even when close file
+
+NEOGIT_DIR = require("oil").get_current_dir()
+
+vim.keymap.set("n", "<leader>cwd", function()
+    NEOGIT_DIR = require("oil").get_current_dir()
+    if not NEOGIT_DIR then
+        NEOGIT_DIR = vim.fn.expand("%:p:h")
+    end
+    print("Neogit Working Directory setted to", NEOGIT_DIR)
+end, { desc = "Change neogit working directory" })
+
+vim.keymap.set({"n", "v"}, "<leader>gg", function ()
+    require("neogit").open({ cwd=NEOGIT_DIR })
+end, { desc = "Show Neogit UI" })
+
+
