f53ccf27727f3d4f0d934fb6f7a61fe5ae906fad
manel <manelzaum@icloud.com>
2026-03-14 13:40:35 -0300

    add: theme and some config to editor.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,13 @@
 require("config.lazy")
 
+vim.cmd.colorscheme "catppuccin-nvim"
+
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
-vim.cmd.colorscheme "catppuccin-nvim"
+vim.o.number = true
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+vim.o.undofile = true
