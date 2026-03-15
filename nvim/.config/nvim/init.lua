266981c4373977db308bb722121098ac24e7f5c7
manel <manelzaum@icloud.com>
2026-03-15 19:30:47 -0300

    active catppuccin.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,3 +4,5 @@ pack.add{{ src = "https://github.com/nvim-lua/plenary.nvim", name = "plenary" }}
 -- Catppuccin | Colorscheme
 pack.add{{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" }}
 
+vim.cmd.colorscheme "catppuccin-nvim"
+
