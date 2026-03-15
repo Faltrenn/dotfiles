3b14eaccf8977abf578629cb09b5f85a3d222ea6
manel <manelzaum@icloud.com>
2026-03-15 19:30:04 -0300

    add plenary.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,6 @@
+-- Plenary | Plugin toolkit | Some plugins depends on it
+pack.add{{ src = "https://github.com/nvim-lua/plenary.nvim", name = "plenary" }}
+
 -- Catppuccin | Colorscheme
 pack.add{{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" }}
 
