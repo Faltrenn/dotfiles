61f15fe7681e842b04baeace38ff6b26ac99940a
manel <manelzaum@icloud.com>
2026-03-15 20:13:19 -0300

    add treesitter.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,16 +5,16 @@ local keymap = vim.keymap
 local pack = vim.pack
 
 -- Plenary | Plugin toolkit | Some plugins depends on it
-pack.add{{ src = "https://github.com/nvim-lua/plenary.nvim", name = "plenary" }}
+pack.add{{ name = "plenary", src = "https://github.com/nvim-lua/plenary.nvim" }}
 
 -- Catppuccin | Colorscheme
-pack.add{{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" }}
+pack.add{{ name = "catppuccin", src = "https://github.com/catppuccin/nvim" }}
 
 vim.cmd.colorscheme "catppuccin-nvim"
 
 -- Oil | File explorer
 
-pack.add{{ src = "https://github.com/stevearc/oil.nvim", name = "oil" }}
+pack.add{{ name = "oil", src = "https://github.com/stevearc/oil.nvim" }}
 
 require("oil").setup({
 	default_file_explorer = true,
@@ -33,7 +33,7 @@ keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Neogit | Tool to handle git
 
-pack.add{{ src = "https://github.com/NeogitOrg/neogit", name = "neogit" }}
+pack.add{{ name = "neogit", src = "https://github.com/NeogitOrg/neogit" }}
 
 require("neogit").setup({
 	kind ="floating",
@@ -48,7 +48,22 @@ keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>")
 
 -- Vim Tmux Navigator | Easy navigation between neovim windows and tmux panels
 
-pack.add{{ src = "https://github.com/christoomey/vim-tmux-navigator" }}
+pack.add{{ name="vim-tmux-navigator", src = "https://github.com/christoomey/vim-tmux-navigator" }}
+
+-- Treesitter | Better code highlight
+pack.add{{ name = "treesitter", src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" }}
+
+require("nvim-treesitter.configs").setup({
+    auto_install = true,
+    indent = { enable = true },
+
+    ignore_install = { "latex" },
+    highlight = {
+	enable = true,
+
+	disable = { "latex" },
+    },
+})
 
 -- Some basic configs
 
