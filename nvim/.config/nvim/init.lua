e2533347b8799103a3e1ff628bb4241ca7f0c0ba
manel <manelzaum@icloud.com>
2026-03-22 19:21:00 -0300

    fix: scrolloff higher than half window.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -204,15 +204,20 @@ keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>") -- Close search highlight
 
 -- ScrollEOF manual
 
+local function get_win_info()
+    return vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
+end
+
 local function get_virtual_empty_lines()
-    local win_info = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
     local total_lines = vim.api.nvim_buf_line_count(0)
+    local win_info = get_win_info()
     local lines_of_text_visible = math.max(0, total_lines - win_info.topline + 1)
     return math.max(0, win_info.height - lines_of_text_visible)
 end
 
 local scroll_eof = function()
-    local offset = vim.o.scrolloff
+    local win_half_height = get_win_info().height / 2
+    local offset = vim.o.scrolloff < win_half_height and vim.o.scrolloff or win_half_height
     local current_row = vim.api.nvim_win_get_cursor(0)[1]
     local total_rows = vim.api.nvim_buf_line_count(0)
     local blank_lines = get_virtual_empty_lines()
